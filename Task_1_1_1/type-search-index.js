7b95ce23fb7d9055c47dd719f4188f4b1ca7a43b
whiteevilbro <whiteevilbro@users.noreply.github.com>
2025-09-07 14:03:40 +0000

    Deploying to gh-pages from @ whiteevilbro/OOP@ff32b0e059f4c179660d8e3e68abc92df908a448 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sys.pro","l":"Heap"},{"p":"sys.pro","l":"HeapSort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"sys.pro","l":"Heap"},{"p":"sys.pro","l":"HeapSort"},{"p":"sys.pro","l":"Main"}];updateSearchResults();
